903e7d3512c1d8788916da5fbda5146de1b1707b
leicas <wspeed@gmail.com>
2015-03-26 13:09:42 +0100

    Add bugfixs and improve stats and current pack of coffee handling

--- cafe.py
+++ cafe.py
@@ -7,6 +7,7 @@ class Listepers(npyscreen.Autocomplete):
         self.handlers.update({curses.ascii.TAB: self.auto_complete})
         self.handlers.update({curses.ascii.NL: self.auto_complete})
         self.handlers.update({curses.ascii.CR: self.auto_complete})
+        self.handlers.update({"^N": self.parent.new_pack})
     def auto_complete(self, input):
         possibilities = filter(lambda x: x.upper().startswith(self.value.upper()), utilisateurs)
         if len(possibilities) is 1:
@@ -34,9 +35,13 @@ def affichage(user):
         total = total + util['account']
     sous.append("----------")
     sous.append("Balance: " + str(-total*0.01))
-    sous.append("Nombre de cafes: " + str(user['nombre']))
-    sous.append("Nombre de paquets: " + str(user['paquets']))
-    sous.append("Cafes par paquet: " + str(user['nombre']//user['paquets']))
+    sous.append("Paquet en cours: " + str(user['current']))
+    ligne = ''
+    ligne += str(user['nombre']) + " cafes"
+    ligne +=" en " + str(user['paquets']) + " paquets"
+    sous.append(ligne)
+    sous.append("Cafes par paquet: " + str((user['nombre']-user['current'])//(user['paquets']-1)))
+
     return sous
 
 
@@ -48,6 +53,18 @@ class myEmployeeForm(npyscreen.ActionFormMinimal):
        self.Name = self.add(TitleListepers, use_two_lines=True,begin_entry_at=0, name='Name')
        #self.Name = self.add(npyscreen.TitleSelectOne, scroll_exit=True, max_height=3, name='Name', values = utilisateurs)
        self.Affichage = self.add(npyscreen.TitlePager, values=sous, use_two_lines=True,begin_entry_at=0, name='Comptes',editable=False)
+    def new_pack(self,input):
+        user = []
+        with open("user.json", "r") as data_file:
+            user = json.load(data_file)
+        user['paquets'] = user['paquets'] + 1
+	user['current'] = 0
+        sous = affichage(user)
+	self.Affichage.values = sous
+  	self.Affichage.display()
+        with open("user.json", "w") as outfile:
+          json.dump(user, outfile, indent=4)
+
     def on_ok(self):
         test = self.Name.value
         user = []
@@ -61,19 +78,7 @@ class myEmployeeForm(npyscreen.ActionFormMinimal):
         user['users'][test]['account']=user['users'][test]['account']-user['prix']
         user['users'][test]['nombre']=user['users'][test]['nombre']+1
         user['nombre'] = user['nombre'] + 1
-#        total = 0
-#        for util in user['users']:
-#            ligne = ''
-#            ligne += util['name']
-#            ligne += " "*(12 - len(util['name']))
-#            ligne += '{0:+}'.format(util['account']*0.01)
-#            ligne += " " * (8-len('{0:+}'.format(util['account']*0.01)))
-#            sous.append(ligne + "Nombre de cafe:" + str(util['nombre']))
-#            total = total + util['account']
-#
-#        sous.append("----------") 
-#        sous.append("Balance: " + str(-total*0.01))
-#        sous.append("Nombre de cafe: " + str(user['nombre']))
+	user['current'] = user['current'] + 1
         sous = affichage(user)
         self.Affichage.values = sous
         self.Name.value = ''
@@ -86,33 +91,15 @@ class myEmployeeForm(npyscreen.ActionFormMinimal):
 class MyApplication(npyscreen.NPSAppManaged):
    def onStart(self):
        self.addForm('MAIN', myEmployeeForm, name='Gestion du Cafe')
-       # A real application might define more forms here.......
    def onInMainLoop(self):
-       #with open("user.json", "r") as data_file:
-        #   user = json.load(data_file)
-       #user['users'][0]['account'] = user['users'][0]['account'] + 1
        pass
 
 if __name__ == '__main__':
     with open("user.json", "r") as data_file:
         user = json.load(data_file)
     utilisateurs = []
-#    sous = []
-#    total = 0
     for util in user['users']:
         utilisateurs.append(util['name'])
-#        ligne = ''
-#        ligne += util['name']
-#        ligne += " "*(12 - len(util['name']))
-#        ligne += '{0:+}'.format(util['account']*0.01)
-#        ligne += " " * (8-len('{0:+}'.format(util['account']*0.01)))
-#        sous.append(ligne + "Nombre de cafe:" + str(util['nombre']))
-#        total = total + util['account']
-#    sous.append("----------") 
-#    sous.append("Balance: " + str(-total*0.01))
-#    sous.append("Nombre de cafes: " + str(user['nombre']))
-#    sous.append("Nombre de paquets: " + str(user['paquets']))
-#    sous.append("Cafes par paquet: " + str(user['nombre']//user['paquets']))
     sous = affichage(user)
     TestApp = MyApplication().run()
 
